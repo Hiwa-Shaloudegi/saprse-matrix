725357094d1a4b4f9decca6f348b9d955a6f714b
Hiwa Shaloudegi <hiwa.shaloudegi@gmail.com>
2021-11-11 15:46:26 +0330

    functions prototype has been written

--- sparse.py
+++ sparse.py
@@ -1,1 +1,50 @@
-ÿþ
+class Function:
+    
+    @staticmethod
+    def densify(matrix):
+        pass
+
+
+    @staticmethod
+    def tranpose(matrix):
+        sparse = Function.make_to_sparse(matrix)
+        pass
+
+
+
+
+    @staticmethod
+    def add(matrix1 ,matrix2):
+        sparse1 = Function.make_to_sparse(matrix1)
+        sparse2 = Function.make_to_sparse(matrix2)
+        pass
+        
+
+
+
+
+
+    @staticmethod
+    def minus(matrix1, matrix2):
+        sparse1 = Function.make_to_sparse(matrix1)
+        sparse2 = Function.make_to_sparse(matrix2)
+        pass
+
+
+
+    @staticmethod
+    def multiply(matrix1, matrix2):
+        sparse1 = Function.make_to_sparse(matrix1)
+        sparse2 = Function.make_to_sparse(matrix2)
+        pass
+
+
+
+
+    
+    @staticmethod
+    def divide(matrix1, matrix2):
+        sparse1 = Function.make_to_sparse(matrix1)
+        sparse2 = Function.make_to_sparse(matrix2)
+        pass
+
